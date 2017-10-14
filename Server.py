c31309064762925e9ddbd1e1ba5f327f4c8fc11e
Freitas26 <victorfreitas26@gmail.com>
2017-10-14 00:26:11 -0300

    Servidor consegue avisar quando os jogadores se conectam

--- Server.py
+++ Server.py
@@ -13,11 +13,16 @@ print ('soquete colocado na porta '+ str(porta))
 soquete.listen(4)
 jogadores = []
 i=0
-#trocar o numero do loop quando terminar os testes!
+
 while 1:
     Scliente, addr=soquete.accept()
     print ('o cliente %s parece ter conectado'%str(addr[0]))
     jogadores.append(Scliente)
     i=i+1
-    aviso=('voce e o jogador %d'% i)
+    aviso=('voce e o jogador %d. aguarde todos os jogadores conectarem'% i)
     jogadores[i-1].send(aviso.encode('ascii'))
+    if i==2:
+        break
+msg='o jogo iniciara agora.'
+for i in jogadores:
+    i.send(msg.encode('ascii'))

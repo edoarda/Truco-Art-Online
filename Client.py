d4808bfdd3e75a4790d6e57a6a3552a43b561717
Freitas26 <victorfreitas26@gmail.com>
2017-10-14 22:24:27 -0300

    Cliente conecta a qualquer lugar e porta

--- Client.py
+++ Client.py
@@ -1,16 +1,16 @@
 import socket
-
+import sys
 
 #tentar criar um socket
 c_sock=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 print ('soquete criado')
 #eis aqui a porta de destino
-hospedeiro=socket.gethostname()
-porta=4001
+destino=sys.argv[1]
+porta=int(sys.argv[2])
 
-print ('tentando acessar a porta'+ str(porta))
+print ('tentando acessar a porta '+ str(porta) +' do local '+ destino)
 #precisamos esperar os 4 jogadores
-c_sock.connect((hospedeiro,porta))
+c_sock.connect((destino,porta))
 print ('conectado ao servidor')
 aviso=c_sock.recv(1024)
 print (aviso.decode('ascii'))

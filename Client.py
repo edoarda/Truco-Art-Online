95b54301c101b658043f416665f60934a92e2ba4
Freitas26 <victorfreitas26@gmail.com>
2017-10-15 21:36:25 -0200

    Cliente com começo de comunicação e jogo(não testado)

--- Client.py
+++ Client.py
@@ -18,5 +18,23 @@ msg=c_sock.recv(1024)
 print (msg.decode('utf-8'))
 msg=c_sock.recv(1024)
 print (msg.decode('utf-8'))
+cartaS[]=msg.split(6)
+for i in range(0,6,2):
+    carta=(cartaS[i],cartaS[i+1])
+    mao[].append(carta)
+
 msg=c_sock.recv(1024)
 print (msg.decode('utf-8'))
+while 1:
+    #cria
+    msg=c_sock.recv(1024)
+    print("Escolha sua ação:\n 1-Jogar carta aberta\n 2-Jogar carta fechada\n 3-Pedir truco\n")
+    resp=input("responda com o numero correspondente:")
+    if int(resp)==1 or int(resp)==2:
+        print ('Escolha a carta a ser jogada:')
+        for i in range (0,len(mao)):#loop para botar as opções na tela
+            print('%d- %s de %s\n'% i,mao[i][0],mao[i][1])
+        resp2=input("responda com o numero correspondente a carta escolhida")
+        opcao=mao.pop(resp2-1)
+        choice= resp +' '+ opcao[0]+' '+opcao[1]
+    c_sock.send(choice.encode('utf-8'))
